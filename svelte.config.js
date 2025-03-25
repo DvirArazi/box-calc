1d2ffd70ff5dd6f915093f46fd88d6013f9208f0
dvirarazi0@gmail.com <dvirarazi0@gmail.com>
2025-03-25 17:29:03 +0200

    x

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,8 @@ const config = {
 	kit: {
 		adapter: adapter({
 			fallback: '404.html',
-			pages: 'build/'
+			pages: 'build',
+			assets: 'build',
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
